62275c0e7d7ad9a8702a80ffc5bce06970274952
Chris Cheetham <chris@cheetham.com>
2023-03-23 12:06:10 -0400

    add doc

--- display_tree.py
+++ display_tree.py
@@ -8,6 +8,13 @@ INDENT = '    '
 
 def display_tree(node, indent='', out=sys.stdout):
     """
+    Each node name is printed on its own line.
+
+    For each level of the tree, an indent is added.
+    The indent of a level is: (level - 1) * indent.
+    An item on level 1 would have no indent.
+    An item on level 3 would have 2 indents.
+
 
     :param Node node:
     :param str indent:
